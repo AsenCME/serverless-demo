c57f18f86f70fc20b0b9ecab2ae38cd87d639e00
Asen Georgiev <asen.connor@abv.bg>
2022-07-11 16:54:05 +0300

    init

---
+++ claims.js
@@ -0,0 +1,29 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Get all claims",
+  });
+});
+
+// get one claim
+app.get("/:id", (req, res, next) => {
+  return res.status(200).json({
+    message: `Get one claim with ID ${req.params.id}`,
+  });
+});
+
+app.put("/:claim_id?", (req, res, next) => {
+  const message = req.params.claim_id
+    ? `Upadte claim with ID ${req.params.claim_id}`
+    : "Create a new claim";
+  return res.status(200).json({ message });
+});
+
+// not found handler
+app.use((req, res, next) => res.status(404).json({ error: "Not Found" }));
+
+// export serverless
+module.exports.handler = serverless(app);
